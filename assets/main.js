eb67d82c6ee265efe1a7ab84eb4a934643a2a850
Andres Simauchi <simauchi3@gmail.com>
2017-01-11 18:17:56 -0500

    Rest of the steps for project

--- assets/main.js
+++ assets/main.js
@@ -3,8 +3,15 @@ $(function() {
       url: 'https://www.codeschool.com/users/simauchi3.json',
       dataType: 'jsonp',
       success: function(response) {
-        $.each(response.courses.completed, function(name, value){
-            $('#badges').append('<div class=".course"></div>');
+        $.each(response.courses.completed, function(index, course){
+            //console.log(course.title);
+            $('#badges').append(
+                '<div class=".course">' +
+                '<h3>'+ course.title + '</h3>' +
+                '<img src=' + course.badge + '>' +
+                '<a href='+ course.url + 'target=\'_blank\' class=\'btn btn-primary\'> See Course </a>' +
+                '</div>'
+            );
         });
       }
     });
