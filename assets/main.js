58cc89ddebc73de23f89644f452df137ce182f72
Andres Simauchi <simauchi3@gmail.com>
2017-01-11 17:19:47 -0500

    Steps 5 and 6 of jQueryBadgesProject
    Made an AJAX request and trying to see what is the contents of the
    response with console.log

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,13 @@
 $(function() {
-
-  // your code will go here
+    $.ajax({
+      url: 'https://www.codeschool.com/users/simauchi3.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        $.each(response, function(){
+            console.log(this);
+            //$('#badges').html('<div></div>').addClass('.course');
+        });
+      }
+    });
 
 });
